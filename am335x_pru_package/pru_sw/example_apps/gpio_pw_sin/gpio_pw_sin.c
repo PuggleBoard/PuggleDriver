f93ef30242b611d0721099b983df3c784da8e086
kbaumanis <kristaps.baumanis@gmail.com>
2013-02-14 23:36:11 +0000

    Adding PRU stuff

---
+++ am335x_pru_package/pru_sw/example_apps/gpio_pw_sin/gpio_pw_sin.c
@@ -0,0 +1,136 @@
+//This code initializes the PRU, changes the GPIO to work with GPIO0_7, creates the sinusoid information for the PRU to work with, and stores it into the PRU's memory.
+
+   /* Driver header file */
+   #include <stdio.h>
+   #include <stdlib.h>
+   #include <prussdrv.h>
+   #include <pruss_intc_mapping.h>
+   #include <math.h>
+
+   #define PRU_NUM     0
+  
+   /* IRQ handler thread */
+   void *pruevtout0_thread(void *arg) {
+       do {
+          prussdrv_pru_wait_event (PRU_EVTOUT_0);
+          prussdrv_pru_clear_event (PRU0_ARM_INTERRUPT);
+       } while (1);
+   }
+  
+   int main (void)
+   {
+	//Variables for sinusoidal data
+	double freq = 367;
+	double omega = 2*M_PI*freq;
+	double sampleRate = 133333.3333333333;
+	int samples = (int)(sampleRate/freq);
+    	unsigned int pwms[2000];
+	int i;
+	FILE *fp;	
+
+	//Initialize all 0s
+	for(i=0; i<2000; i++){
+		pwms[i] = 0;
+	}
+
+	//Eports GPIO0_7
+	if ((fp=fopen("/sys/class/gpio/export", "w"))==NULL){
+		printf("Cannot open File\n");
+		return(1);
+	}
+	fprintf(fp,"7");
+	fclose(fp);
+
+	//Enables GPIO0_7 for output
+	if ((fp=fopen("/sys/class/gpio/gpio7/direction", "w"))==NULL){
+		printf("Cannot open File\n");
+		return(1);
+	}
+	fprintf(fp,"out");
+	fclose(fp);
+
+	//Creates the sinusoid data to be pushed into the PRU's memory. (DO NOT USE BELOW 67 HZ)
+	//There is not enough memory on the PRU
+	
+	pwms[0] = (samples+1)*4;	
+
+	for(i=1; i<=samples;i++){
+		if(i < 2000){
+			pwms[i] = 250+(int)(25*sin(omega*(i-1)/sampleRate));
+		}
+	}
+
+	/* Initialize structure used by prussdrv_pruintc_intc   
+	   PRUSS_INTC_INITDATA is found in pruss_intc_mapping.h */
+	tpruss_intc_initdata pruss_intc_initdata = PRUSS_INTC_INITDATA;
+
+	/* Allocate and initialize memory */
+	prussdrv_init();       
+	prussdrv_open(PRU_EVTOUT_0);
+ 
+	/* Map PRU's INTC */
+	prussdrv_pruintc_init(&pruss_intc_initdata);
+
+	/* load array on PRU */
+	prussdrv_pru_write_memory(PRUSS0_PRU0_DATARAM, 0, pwms, sizeof(unsigned int)*(samples+1));
+
+    /* read what we just stored */
+    int value = 0;
+    printf("first element we stored = %i\n", pwms[0]);
+    value = prussdrv_pru_read_memory(PRUSS0_PRU0_DATARAM, 0);
+    printf("what we read from PRU = %i\n", value);
+
+	/* Load and execute binary on PRU */
+	prussdrv_exec_program(PRU_NUM, "./gpio_pw_sin.bin");
+
+	printf("Frequency=%f\n",freq);
+	fflush(stdout);
+	scanf("%lf", &freq);
+	if(freq < 67){
+		printf("Frequency: %f is too low. Enter a number higher than 67\n", freq);
+		freq = 67;
+	}
+	printf("Frequency=%f\n",freq);
+	fflush(stdout);
+
+	while(1){
+		if(freq >=67){
+			omega = 2*M_PI*freq;
+			samples = (int)(sampleRate/freq);
+			pwms[0] = (samples+1)*4;
+			for(i=1; i<=samples;i++){
+				if(i < 2000){
+					pwms[i] = 250+(int)(25*sin(omega*(i-1)/sampleRate));
+				}
+			}
+			
+			/* load array on PRU */
+			prussdrv_pru_write_memory(PRUSS0_PRU0_DATARAM, 0, pwms, sizeof(unsigned int)*(samples+1));
+
+            /* read what we just stored */
+            printf("first element we stored = %i\n", pwms[0]);
+            value = prussdrv_pru_read_memory(PRUSS0_PRU0_DATARAM, 0);
+            printf("what we read from PRU = %i\n", value);
+
+			/* Load and execute binary on PRU */
+			prussdrv_exec_program(PRU_NUM, "./gpio_pw_sin.bin");
+			
+		}
+		scanf("%lf", &freq);
+		if(freq < 67){
+			printf("Frequency: %f is too low. Enter a number higher than 67\n", freq);
+			freq = 67;
+		}
+		printf("Frequency=%f\n",freq);
+		fflush(stdout);	
+	}
+	/* Wait for event completion from PRU */
+	prussdrv_pru_wait_event (PRU_EVTOUT_0);  // This assumes the PRU generates an interrupt
+                                                // connected to event out 0 immediately before halting
+  
+	/* Disable PRU and close memory mappings */
+	prussdrv_pru_disable(PRU_NUM);
+	prussdrv_exit ();
+
+	return(0);
+    }
---
+++ am335x_pru_package/pru_sw/example_apps/gpio_pw_sin/gpio_pw_sin.p
@@ -0,0 +1,61 @@
+
+#define GPIO1 0x44e07000
+#define GPIO_CLEARDATAOUT 0x190
+#define GPIO_SETDATAOUT 0x194
+#define MEM_START 0x00000000
+
+//set ARM such that PRU can write to GPIO
+LBCO r0, C4, 4, 4
+CLR r0, r0, 4
+SBCO r0, C4, 4, 4
+
+//r1 = index of array
+MOV r1, 4
+//r21 = end count for array (if delay)
+MOV r21, 496
+//r22 = subtracts off the first 11 instructions for delay (simplification)
+MOV r22, 11
+//r23 = subtracts off the first 9 instructions for delay2 (simplification)
+MOV r23, 9
+//r24 = end count for array (if delay2)
+MOV r24, 495
+//r19 = start of array (first component is length)blinker
+MOV r19, MEM_START
+
+//loads the next array value into r2 to be played and updates index
+LOAD_VALUE:
+	LBBO r18, r19, 0, 4
+	LBBO r2, r19, r1, 4
+	SUB r2, r2, r22
+	SUB r10, r2, r23
+	ADD r1, r1, 4
+	QBNE PWM_ON, r1, r18
+	MOV r1, 4
+
+//turns on GPIO7 pin
+PWM_ON:
+	MOV r3, 1<<7
+	MOV r5, 0
+	QBEQ DELAY2, r2, 0
+	MOV r4, GPIO1 | GPIO_SETDATAOUT
+	SBBO r3, r4, 0, 4
+
+//delay before turning PWM approximation off
+DELAY:
+	ADD r5, r5, 1
+	QBEQ LOAD_VALUE, r5, r21
+	QBNE DELAY, r5, r2
+
+//turns off GPIO7 pin
+PWM_OFF:
+	MOV r3, 1<<7
+	MOV r4, GPIO1 | GPIO_CLEARDATAOUT
+	SBBO r3, r4, 0, 4
+	QBA DELAY
+
+//delay before loading new value and turning PWM approximation on
+DELAY2:
+	ADD r5, r5, 1
+	QBEQ LOAD_VALUE, r5, r24
+	QBNE DELAY2, r5, r10
+	QBA PWM_OFF
