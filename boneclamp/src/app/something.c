d7c6298cb9870ac24df0301a63687fb8c4f5e18b
yapatel525 <yapatel@gatech.edu>
2013-03-18 21:47:56 -0400

    Updating licensing information

--- boneclamp/src/app/something.c
+++ boneclamp/src/app/something.c
@@ -1,8 +1,23 @@
+/*
+	 -------------------------------------------------------------------------
+
+	 This file is part of the BoneClamp Data Conversion and Processing System
+	 Copyright (C) 2013 BoneClamp
+
+	 -------------------------------------------------------------------------
+
+	Filename: something.c
+
+	To the extent possible under law, the author(s) have dedicated all copyright and related and neighboring rights to this software to the public domain worldwide. This software is distributed without any warranty.
+
+	You should have received a copy of the CC Public Domain Dedication along with this software. If not, see <http://creativecommons.org/licenses/by-sa/3.0/legalcode>.
+ */
+
 #include "boneclamp.h"
 
 int main()  {
-    //DO SOMETHING!
-    
-    return 0;
+	//DO SOMETHING!
+
+	return 0;
 }
 
