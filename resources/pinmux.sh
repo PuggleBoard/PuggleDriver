6e4ef73c862d81defdbdb73764bcda8e643f5dae
Yogi Patel <yapatel@gatech.edu>
2013-09-25 23:57:16 +0000

    Fixing pinmuxes

--- resources/pinmux.sh
+++ resources/pinmux.sh
@@ -1,13 +1,15 @@
 #!/bin/sh
 
+rm *.dtbo
 set -x
 set -e
 export SLOTS=/sys/devices/bone_capemgr.8/slots
 export PINS=/sys/kernel/debug/pinctrl/44e10800.pinmux/pins
-dtc -O dtb -o ~/Dev/PuggleDriver/resources/puggle_4ch-00A0.dtbo -b 0 -@ ~/Dev/PuggleDriver/resources/puggle_4ch.dts
-cp ~/Dev/PuggleDriver/resources/puggle_4ch-00A0.dtbo /lib/firmware/
+dtc -O dtb -o puggle_4ch-00A0.dtbo -b 0 -@ puggle_4ch.dts
+cp puggle_4ch-00A0.dtbo /lib/firmware/
 cat $SLOTS
 echo puggle_4ch > $SLOTS
-dmesg | tail
 cat $SLOTS
-cat $PINS | grep 0E0
+cat $PINS | grep 57
+cat $PINS | grep 56
+cat $PINS | grep 58
