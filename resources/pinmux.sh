658e0d5311b76b4ce0659e55abc9c435246f6297
jonnew <jnewman6@gatech.edu>
2013-10-19 18:14:03 +0000

    updating pinmux, merging memory transfer

--- resources/pinmux.sh
+++ resources/pinmux.sh
@@ -9,16 +9,4 @@ cat $SLOTS
 echo puggle-4ch > $SLOTS
 #dmesg | tail
 cat $SLOTS
-cat $PINS | grep 8a0
-cat $PINS | grep 8a4
-cat $PINS | grep 8a8
-cat $PINS | grep 8aC
-cat $PINS | grep 8b0
-cat $PINS | grep 8b4
-cat $PINS | grep 8b8
-cat $PINS | grep 8bC
-cat $PINS | grep 8e0
-cat $PINS | grep 8e4
-cat $PINS | grep 8e8
-cat $PINS | grep 8eC
-
+./check.sh
