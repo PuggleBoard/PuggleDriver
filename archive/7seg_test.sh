84ad13056a764b5a4326c0ea457f2f28253d14ac
yapatel <yapatel@gatech.edu>
2013-10-24 05:29:01 +0000

    cleaning up archives

--- archive/7seg_test.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash -e
-
-for i in {0..9999}
-    do
-        echo `printf %04d ${i%.*}`
-        echo `printf %04d ${i%.*}` >/dev/pruss7seg
-    done
-
--- archive/old_examples/build.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash -e
-
-make | egrep --color -i '[1-9] error|'
-
