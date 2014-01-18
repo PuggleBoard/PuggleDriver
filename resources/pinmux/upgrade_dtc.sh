749c13bb6a73756385f0d004d85bde909c8ef1e8
yapatel <yapatel@gatech.edu>
2014-01-18 17:24:48 -0500

    upgrading dtc version for pinmux and pinmux check script

---
+++ resources/pinmux/upgrade_dtc.sh
@@ -0,0 +1,23 @@
+##
+##	 -------------------------------------------------------------------------
+##
+##	 This file is part of the Puggle Data Conversion and Processing System
+##	 Copyright (C) 2013 Puggle
+##
+##	 -------------------------------------------------------------------------
+##
+##	Written in 2013 by: Yogi Patel <yapatel@gatech.edu>
+##
+##	To the extent possible under law, the author(s) have dedicated all copyright
+##	and related and neighboring rights to this software to the public domain
+##	worldwide. This software is distributed without any warranty.
+##
+##	You should have received a copy of the CC Public Domain Dedication along with
+##	this software. If not, see <http://creativecommons.org/licenses/by-sa/3.0/legalcode>.
+##
+
+#!/bin/bash
+
+wget -c --no-check-certificate https://raw.github.com/RobertCNelson/tools/master/pkgs/dtc.sh
+chmod +x dtc.sh
+./dtc.sh
